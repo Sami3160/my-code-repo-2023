50c4dba1065019e6dac0cdc365608f373cabcaec
Sami3160 <samibhadgaonkar@gmail.com>
2023-11-15 21:28:07 +0530

    just added some uncut lines

--- dsa/symmetricmatrix.c
+++ dsa/symmetricmatrix.c
@@ -3,6 +3,11 @@ int main()
 {
 	int m,n,i,j;
 	int arr[m][n];
+	printf("Enter the dimensions of matric (m * n) : \nm : ");
+	scanf("%d",m);
+	printf("\nn : ");
+	scanf("%d",n);
+	printf("\nNow enter the matrix elements : \n");
 	
 	for(i=0;i<m;i++){
 		for(j=0;j<n;j++){
