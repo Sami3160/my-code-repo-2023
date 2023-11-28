119572948f1cb21dec05ff35464da71c3df6eb80
Sami3160 <samibhadgaonkar@gmail.com>
2023-11-28 23:29:31 +0530

    async function

--- js/Sync Async/asyncFunction.js
+++ js/Sync Async/asyncFunction.js
@@ -13,3 +13,11 @@ function regularFunction() {
   // Using the async function
   a=asyncFunction()
   console.log(a);
+  //Promise { 'Hello, World!' } would be output, because async returns object of promise
+
+  asyncFunction().then(result =>{
+    console.log(result,"from then()")
+  }).catch(reject =>{
+    console.log("Error found")
+  });
+//similar like promise we can use async coz async functions also returns promise object
