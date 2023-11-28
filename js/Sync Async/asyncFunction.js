01a9a85ba9c6124e9f1e230a60b430c32c47e892
Sami3160 <samibhadgaonkar@gmail.com>
2023-11-28 23:19:46 +0530

    new changes

---
+++ js/Sync Async/asyncFunction.js
@@ -0,0 +1,15 @@
+function regularFunction() {
+    return "Hello, World!";
+  }
+  
+  // Async function
+  async function asyncFunction() {
+    return "Hello, World!";
+  }
+  
+  // Using the regular function
+  console.log(regularFunction()); // Prints: Hello, World!
+  
+  // Using the async function
+  a=asyncFunction()
+  console.log(a);
--- js/Sync Async/syncExample.js
+++ js/Sync Async/syncExample.js
@@ -3,3 +3,4 @@ console.log("Middle");
 console.log("End");
 
 //here execution is start->middle->end in sequentioal manner
+//one instruction starts only after first one is ended
