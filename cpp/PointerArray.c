41f7acada82974fbfc9268533772828744670e89
Sami3160 <110652247+Sami3160@users.noreply.github.com>
2024-02-29 20:52:55 +0530

    Create PointerArray.c

---
+++ cpp/PointerArray.c
@@ -0,0 +1,22 @@
+
+
+#include<stdio.h>
+int main(){
+   int  i;
+   int arr[10];
+   int *ptr;
+   ptr=arr;
+   printf("Enter 5 Elements ");
+   for(i=0;i<5;i++)
+   { 
+   	  scanf("%d",ptr);
+		 ptr++; 
+   }
+   ptr=arr;
+    printf("Entered Numbers:\n");
+    for(i=0;i<5;i++)
+   { 
+   	  printf("%d\n",*ptr);
+		 ptr++; 
+   }
+}
