f6ec0ef598a87e03f6691ca9a8c125ecd7ca70be
Sami3160 <samibhadgaonkar@gmail.com>
2025-08-10 23:21:03 +0530

    ls logic improved, clear all option added, help opetion added

--- shell scripting/todo.sh
+++ shell scripting/todo.sh
@@ -4,39 +4,13 @@ DATA_DIR="$HOME/.local/share/mytodo"
 mkdir -p $DATA_DIR
 DATA_FILE="$DATA_DIR/data.csv"
 USER_FILE="$DATA_DIR/user.txt"
-name=""
-password=""
+name="samii"
+password="1234"
 if [ ! -n "$1" ]; then
     echo "Not enough arguments!"
     return 0;
 fi
-# if [ ! -e "$USER_FILE" ] || [ ! -s "$USER_FILE" ]; then
-#     touch $USER_FILE
-#     # echo "Register urself"
-#     while true;do
-#         read -p "Please enter your name: " name
-#         name=$(echo -n "$name" | sed 's/^ *//;s/ *$//');
-#         if [ -n "$name" ]; then
-#             break
-#         fi
-#         echo "Error: Name should not be empty"
-#     done
-#     while true;do
-#         read -p "Please enter your password: " password
-#         password=$(echo -n "$password" | sed 's/^ *//;s/ *$//');
-#         if [ -n "$password" ]; then
-#             break
-#         fi
-#             echo "Error: Password should not be empty"
-#     done
-#     # name=$("echo $name" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
-#     # password=$("echo $password" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
-#     password=$(sha1sum <<< $password)
-#     echo "$name $password" > "$USER_FILE"
-#     # echo "Enter your name"
-#     echo "User created!"
-#     echo "Welcome $name"
-# fi
+ echo "Welcome $name"
 if [ ! -e "$DATA_FILE" ]; then
     touch $DATA_FILE
     echo "id,title,description,date,status" >> $DATA_FILE;
@@ -78,30 +52,54 @@ case "$1" in
         echo -e "$id,$title,$description,$date,pending" >> $DATA_FILE
 
     ;;
-    "-ls")
-        filter=""
-        if [ $(cut -c 1-6 <<< "$2") == "--sort" ]; then
-            filter=$(cut -c 7- <<< "$2")
-            case $filter in 
-                "status")
-                ;;
-                "title")
-                ;;
-                "date")
-                ;;
+    #new listing logic
+   "-ls")
+        sort_key=""
+        if [[ "$2" == --sort=* ]]; then
+            sort_field=$(echo "$2" | cut -d'=' -f2)
+            case "$sort_field" in
+                "status") sort_key=5 ;;
+                "title") sort_key=2 ;;
+                "date") sort_key=4 ;;
                 *)
-                echo "Error: Invalid --sort argument value"
-                return 0
+                    echo "Error: Invalid --sort argument value. Use 'status', 'title', or 'date'."
+                    exit 1
                 ;;
             esac
+            (head -n 1 "$DATA_FILE" && tail -n +2 "$DATA_FILE" | sort -t, -k"$sort_key") | column -t -s,
+        else
+            cat "$DATA_FILE" | column -t -s,
         fi
-    cat $DATA_FILE | column -t -s ,
     ;;
     "-rm")
     ;;
     "--clear-all")
-        read -p "Password needed for clear all " password
-        ;;
+        read -sp "Password needed to clear all tasks: " user_password
+        echo
+        if [ "$user_password" == "$password" ]; then
+            head -n 1 "$DATA_FILE" > "$DATA_FILE.tmp" && mv "$DATA_FILE.tmp" "$DATA_FILE"
+            echo "All tasks have been cleared."
+        else
+            echo "Incorrect password. Action aborted."
+        fi
+    ;;
+    "--help")
+        echo "Usage: mytodo [command] [options]"
+        echo
+        echo "Commands:"
+        echo "  -t \"<title>\" [-d \"<description>\"]  Add a new task."
+        echo "  -d \"<description>\" -t \"<title>\"  Add a new task (alternative order)."
+        echo "  -ls [--sort=<field>]              List all tasks. Sort by 'status', 'title', or 'date'."
+        echo "  -rm <ID>                          Remove a task by its ID."
+        echo "  -u <ID> <new_status>              Update the status of a task (e.g., 'done', 'in-progress')."
+        echo "  --clear-all                       Remove all tasks (requires password)."
+        echo "  --help                            Show this help message."
+        echo
+        echo "Example:"
+        echo "  mytodo -t \"Buy milk\" -d \"Get 2% milk from the store\""
+        echo "  mytodo -ls --sort=status"
+        echo "  mytodo -u f4c3d2 done"
+    ;;
     *)
     echo "yohoho"
     ;;
