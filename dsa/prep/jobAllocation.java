c9ad6565617c0c64fec03aff7d03977392fcf487
Sami3160 <samibhadgaonkar@gmail.com>
2025-09-01 23:08:56 +0530

    random problem

---
+++ dsa/prep/jobAllocation.java
@@ -0,0 +1,14 @@
+import javax.print.attribute.standard.JobStateReasons;
+
+public class jobAllocation {
+    public int solution(int[] jobs, int k){
+        
+        return 0;
+    }
+    public static void main(String[] args) {
+        jobAllocation jb=new jobAllocation();
+        int [] jobs={1, 2, 4, 7, 8  };
+        int k=2  ;
+        System.out.println(jb.solution(jobs,k));
+    }
+}
